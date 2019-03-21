d2ee91858555a61c8964179f7f206c5ba636a613
Serge Ayissi <ayissi.serge@gmail.com>
2019-03-20 22:04:44 -0400

    Create 1_install_ssl_letsencrypt.sh

---
+++ ssl/1_install_ssl_letsencrypt.sh
@@ -0,0 +1,7 @@
+# If not installed, install Let's Encrypt to get the SSL certificate for free (for 90 days) and then renew
+
+# In Plesk go to Server Management and choose the websites you would like to install SSL certificates on
+
+# Click: Secure Websites
+See provided picture
+
